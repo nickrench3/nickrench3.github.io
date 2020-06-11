c7d4652f43d19b4280031d5d8965779521ba13f5
Nick Rentschler <rentschler.nickolas@ic.edu>
2020-06-10 20:38:33 -0500

    Updated code and added JS

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
 }
+// Script for Tab FAQ
+function openFAQ(evt, cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  var activebtn = document.getElementsByClassName("faqbtn");
+  for (i = 0; i < x.length; i++) {
+    activebtn[i].className = activebtn[i].className.replace(" w3-blue", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-blue";
+}
+var mybtn = document.getElementsByClassName("faqbtn")[0];
+mybtn.click();
