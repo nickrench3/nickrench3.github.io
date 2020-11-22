2799116bde53ae4c8ad0eff5ddae694f38cfaa7d
Nick Rentschler <rentschler.nickolas@ic.edu>
2020-11-21 19:17:34 -0600

    Update script

--- Scripts/script.js
+++ Scripts/script.js
@@ -8,7 +8,6 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
 }
-
 // Script for Tab FAQ
 function openFAQ(evt, cityName) {
   var i;
